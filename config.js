ee9b612c38899c16f1a60c1ef28295208ab608e9
Adam12909 <115569901+Adam12909@users.noreply.github.com>
2023-01-27 18:15:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -70,7 +70,7 @@ global.mess = {
     error: 'An error occurd!',
     banned: 'You are *Banned* fron using commands!',
     bangc: 'This Group is *Banned* from using Commands!',
-    nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+    nonsfw: 'Dont be a pervert! This is not a NSFW enabled group!'
     
 }
 
