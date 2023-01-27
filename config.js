80cf5c3266643b111c8fab3eaf024b54b04a4db2
Adam12909 <115569901+Adam12909@users.noreply.github.com>
2023-01-27 18:14:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ global.mess = {
     jobdone: 'Job done...',
     useradmin: 'Sorry, only *Group Admins* can use this command!',
     botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
-    botowner: 'Only my *Owner* can use this command, Baka!',
+    botowner: 'Only my *Owner* can use this command!',
     grouponly: 'This command is only made for *Groups*, Baka!',
     privateonly: 'This command is only made for *Private Chat*, Baka!',
     botonly: 'Only the *Bot itself* can use this command!',
